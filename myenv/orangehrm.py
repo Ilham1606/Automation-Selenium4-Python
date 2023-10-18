4ff08dea0c5ddc1b2c94d37d2a4f52edbf1e041e
Ilham1606 <16ilhamapriansyah@gmail.com>
2023-10-18 22:09:51 +0700

    Update code in myenv/orangehrm.py

--- myenv/orangehrm.py
+++ myenv/orangehrm.py
@@ -28,7 +28,7 @@ expected_text = driver.find_element(By.TAG_NAME, "h6").text
 assert expected_text, f"Expected '{expected_text}'"
 print(f"Expected Text is [", expected_text,"]")
 
-driver.find_element(By.XPATH, "//span[text()='Admin']").click()
+#driver.find_element(By.XPATH, "//span[text()='Admin']").click()
 
 # for i in range(2):
 #     delete_record_elements = f"//div[@class='oxd-table-cell-actions']//i[@class='oxd-icon bi-trash-{i+1}']"
